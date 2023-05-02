dff3214665501532f063f09e242aa3ff4ce0665b
Jainy Sanchez <104104065+JainySanchez@users.noreply.github.com>
2023-05-01 21:08:28 -0500

    Cambio de Proyectos

--- src/Components/FormMyProjects.js
+++ src/Components/FormMyProjects.js
@@ -1,83 +1,70 @@
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 function FormMyProjects() {
   return (
-    <div style={{ width: "800px" }}>
-      <Card style={{ width: "800px" }}>
-        <Card.Header as="h5">Nuevo Proyecto</Card.Header>
+    
+    <div>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
         <Card.Body>
           <div>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Nombre"
-              className="mb-3"
-            >
-              <Form.Control placeholder="nameProject" />
-            </FloatingLabel>
-            <FloatingLabel
-              controlId="floatingTextarea2"
-              label="Descripción Detallada"
-            >
-              <Form.Control
-                as="textarea"
-                placeholder="Leave a comment here"
-                style={{ height: "100px" }}
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
               />
-            </FloatingLabel>
-            <br></br>
-            <Form.Select aria-label="Default select example">
-              <option>Seleecione la empresa</option>
-              <option value="Empresa 1">Empresa 1</option>
-              <option value="Empresa 2">Empresa 2</option>
-              <option value="Empresa 3">Empresa 3</option>
-            </Form.Select>
-            <br></br>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Fecha de Inicio"
-              className="mb-3"
-            >
-              <Form.Control placeholder="nameProject" />
-            </FloatingLabel>
-            <br></br>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Fecha Final"
-              className="mb-3"
-            >
-              <Form.Control placeholder="nameProject" />
-            </FloatingLabel>
-            <br></br>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Costo Estimado"
-              className="mb-3"
-            >
-              <Form.Control placeholder="nameProject" />
-            </FloatingLabel>
-            <br></br>
-            <Form.Select aria-label="Default select example">
-              <option>Seleecione las Tareas</option>
-              <option value="Tarea 1">Tarea 1</option>
-              <option value="Tarea 2">Tarea 2</option>
-              <option value="EmpTarearesa 3">Tarea 3</option>
-            </Form.Select>
-            <br></br>
-            <Form.Select aria-label="Default select example">
-              <option>Seleecione el hito</option>
-              <option value="Hito 1">Hito 1</option>
-              <option value="Hito 2">Hito 2</option>
-              <option value="Hito 3">Hito 3</option>
-            </Form.Select>
-            <br></br>
-            <br></br>
-            <Button variant="outline-secondary" size="lg">
-              Guardar Proyecto
-            </Button>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </div>
           </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
+        <Card.Body>
+          <div>
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
+              />
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </div>
+          </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
+        <Card.Body>
+          <div>
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
+              />
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </div>
+          </div>
+          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
     </div>
--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -1,81 +1,177 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import NewProject from "./NewProject";
-import { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
-function MyProjects() {
-    const [showNewProject, setNewProject] = useState(false);
+function MyProjects(props) {
+  const [showNewProject, setNewProject] = useState(false);
 
-    const handleClick = () => {
-        setNewProject(true);
-    }
+  const handleClick = () => {
+    setNewProject(true);
+  };
 
-    const handleCloseModal = () => {
-        showNewProject(false);
-    }
+  const handleCloseModal = () => {
+    setNewProject(false);
+  };
 
-    return (
-        <div>
-            <div style={{ "display": "flex", "flex-direction": "column", 'width': '60rem' }}>
-                <Card>
-                    <Card.Body>This is some Projects</Card.Body>
-                    <Button onClick={handleClick} variant="outline-primary">Create New Project</Button>{showNewProject && <NewProject onClose={handleCloseModal} />}
-                </Card>
-                <br></br>
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    fechaInicial: "",
+    fechaFinal: "",
+    costoEstimado: "",
+  });
 
-                <Card>
-                    <Card.Header as="h5">Project 127681</Card.Header>
-                    <Card.Body>
-                        <div>
-                            <Card.Title>Backend Developer</Card.Title>
-                            <div style={{ 'display': 'flex', 'flex-direction': 'row' }}>
-                                <Card.Img variant="top" src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png" style={{ 'width': '100px' }} />
-                                <Card.Text>
-                                    With supporting text below as a natural lead-in to additional content.
-                                </Card.Text>
-                            </div>
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-                        </div>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br></br>
-                <Card>
-                    <Card.Header as="h5">Project 127681</Card.Header>
-                    <Card.Body>
-                        <div>
-                            <Card.Title>Backend Developer</Card.Title>
-                            <div style={{ 'display': 'flex', 'flex-direction': 'row' }}>
-                                <Card.Img variant="top" src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png" style={{ 'width': '100px' }} />
-                                <Card.Text>
-                                    With supporting text below as a natural lead-in to additional content.
-                                </Card.Text>
-                            </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit(formData);
+  };
 
-                        </div>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br></br>
-                <Card>
-                    <Card.Header as="h5">Project 127681</Card.Header>
-                    <Card.Body>
-                        <div>
-                            <Card.Title>Backend Developer</Card.Title>
-                            <div style={{ 'display': 'flex', 'flex-direction': 'row' }}>
-                                <Card.Img variant="top" src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png" style={{ 'width': '100px' }} />
-                                <Card.Text>
-                                    With supporting text below as a natural lead-in to additional content.
-                                </Card.Text>
-                            </div>
+  return (
+    <div>
+      <Card>
+        <Card.Body>This is some Projects</Card.Body>
+        <Button onClick={handleClick} variant="outline-primary">
+          Create New Project
+        </Button>
+      </Card>
+      <br></br>
+      <Modal show={showNewProject} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nuevo Proyecto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Nombre"
+                className="mb-3"
+              >
+                <Form.Control placeholder="nameProject" />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingTextarea2"
+                label="Descripción Detallada"
+              >
+                <Form.Control
+                  as="textarea"
+                  placeholder="Leave a comment here"
+                  style={{ height: "100px" }}
+                />
+              </FloatingLabel>
+              <br></br>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Fecha de Inicio"
+                className="mb-3"
+              >
+                <Form.Control placeholder="fechaInicio" />
+              </FloatingLabel>
+              <br></br>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Fecha Final"
+                className="mb-3"
+              >
+                <Form.Control placeholder="fechaFinal" />
+              </FloatingLabel>
+              <br></br>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Costo Estimado"
+                className="mb-3"
+              >
+                <Form.Control placeholder="costoEstimado" />
+              </FloatingLabel>
+            
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-                        </div>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
+        <Card.Body>
+          <div>
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
+              />
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
             </div>
-        </div>
-    );
+          </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
+        <Card.Body>
+          <div>
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
+              />
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </div>
+          </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <Card>
+        <Card.Header as="h5">Project 127681</Card.Header>
+        <Card.Body>
+          <div>
+            <Card.Title>Backend Developer</Card.Title>
+            <div style={{ display: "flex", "flex-direction": "row" }}>
+              <Card.Img
+                variant="top"
+                src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                style={{ width: "100px" }}
+              />
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </div>
+          </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default MyProjects;
