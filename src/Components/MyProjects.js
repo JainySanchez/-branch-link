4bf98a19c79a9b338715214005a01872972b1a65
Bryan <bryan@latamready.com>
2023-07-11 11:05:00 -0500

    Actualización visual de proyectos

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -2,46 +2,117 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import NavbarTop from "./NavbarTop";
 import NavbarLeft from "./NavbarLeft";
+import axios from "axios";
 
+// Componente funcional MyProjects
 function MyProjects(props) {
+  const [Nombre, setNombre] = useState("");
+  const [Descripcion, setDesc] = useState("");
+  const [FechaInicio, setFechaIni] = useState("");
+  const [FechaFinalizacion, setFechaFin] = useState("");
+  const [CostoEstimado, setCosto] = useState("");
+
+  // Estado para controlar la visibilidad del modal para crear un nuevo proyecto
   const [showNewProject, setNewProject] = useState(false);
-  const [projectsData, setProjectsData] = useState([]); // variable de estado para los datos de proyectos
+  // Estado para almacenar los datos de los proyectos
+  const [projectsData, setProjectsData] = useState([]);
+  // Estado para almacenar el ID del proyecto en modo de edición
+  const [editProjectId, setEditProjectId] = useState(null);
+
+  // Manejador para abrir el modal para crear un nuevo proyecto
   const handleClick = () => {
     setNewProject(true);
   };
 
+  // Manejador para cerrar el modal
   const handleCloseModal = () => {
     setNewProject(false);
   };
 
+  // Manejador para abrir el formulario de edición de un proyecto
+  const handleEdit = (projectId) => {
+    setEditProjectId(projectId);
+    setNewProject(true);
+  };
+
+  // Estado inicial y manejo de cambios en el formulario
   const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-    fechaInicial: "",
-    fechaFinal: "",
-    costoEstimado: "",
+    Nombre: "",
+    Descripcion: "",
+    FechaInicio: "",
+    FechaFinalizacion: "",
+    CostoEstimado: "",
   });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-    console.log(formData);
+  // Manejador para enviar el formulario de creación o edición de proyecto
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (editProjectId) {
+        // Realiza la solicitud PUT a tu API para actualizar el proyecto
+        const response = await axios.put(`http://localhost:5000/proyectos/${editProjectId}`, {
+          Nombre,
+          Descripcion,
+          FechaInicio,
+          FechaFinalizacion,
+          CostoEstimado,
+        });
+        if (response.status === 200) {
+          const updatedProject = response.data;
+          setProjectsData(projectsData.map((project) => (project.id === updatedProject.id ? updatedProject : project)));
+          console.log("Proyecto actualizado correctamente");
+        } else {
+          // Manejo de errores
+          console.log("Error al actualizar el proyecto");
+        }
+      } else {
+        // Realiza la solicitud POST a tu API para crear un nuevo proyecto
+        const response = await axios.post("http://localhost:5000/proyectos", {
+          Nombre,
+          Descripcion,
+          FechaInicio,
+          FechaFinalizacion,
+          CostoEstimado,
+        });
+        if (response.status === 200) {
+          const newProject = response.data;
+          setProjectsData([...projectsData, newProject]);
+          console.log("Proyecto creado correctamente");
+        } else {
+          // Manejo de errores
+          console.log("Error al crear el proyecto");
+        }
+      }
+      resetFormData();
+      handleCloseModal();
+      setEditProjectId(null); // Restablece el ID del proyecto en modo de edición
+    } catch (error) {
+      console.log("Error de conexión con el servidor:", error);
+    }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setProjectsData([...projectsData, formData]); // actualiza la variable de estado con los datos del proyecto agregado
-    resetFormData();
-    handleCloseModal();
-    // props.onSubmit(formData);
+  // Manejador para eliminar un proyecto
+  const handleDelete = async (projectId) => {
+    try {
+      // Realiza la solicitud DELETE a tu API
+      const response = await axios.delete(`http://localhost:5000/proyectos/${projectId}`);
+      if (response.status === 200) {
+        setProjectsData(projectsData.filter((project) => project.id !== projectId));
+        console.log("Proyecto eliminado correctamente");
+      } else {
+        // Manejo de errores
+        console.log("Error al eliminar el proyecto");
+      }
+    } catch (error) {
+      console.log("Error de conexión con el servidor:", error);
+    }
   };
+
+  // Función para restablecer el formulario
   const resetFormData = () => {
     setFormData({
       name: "",
@@ -51,7 +122,26 @@ function MyProjects(props) {
       costoEstimado: "",
     });
   };
-  console.log(projectsData);
+
+  // Realiza una solicitud GET al cargar el componente para obtener los proyectos existentes
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/proyectos");
+        if (response.status === 200) {
+          const data = response.data;
+          setProjectsData(data);
+        } else {
+          // Manejo de errores
+          console.log("Error al obtener los proyectos");
+        }
+      } catch (error) {
+        console.log("Error de conexión con el servidor:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []); // Se pasa un array vacío para que solo se ejecute una vez al cargar el componente
 
   return (
     <div>
@@ -64,7 +154,6 @@ function MyProjects(props) {
 
         <div>
           <Card style={{ width: "1000px" }}>
-            <Card.Body>Algunos proyectos</Card.Body>
             <Button onClick={handleClick} variant="outline-primary">
               Crear nuevo Proyecto
             </Button>
@@ -83,8 +172,8 @@ function MyProjects(props) {
                       type="text"
                       id="name"
                       name="name"
-                      value={formData.name}
-                      onChange={handleInputChange}
+                      value={Nombre}
+                      onChange={(event) => setNombre(event.target.value)}
                     />
                   </FloatingLabel>
 
@@ -95,8 +184,8 @@ function MyProjects(props) {
                       style={{ height: "100px" }}
                       id="description"
                       name="description"
-                      value={formData.description}
-                      onChange={handleInputChange}
+                      value={Descripcion}
+                      onChange={(event) => setDesc(event.target.value)}
                     />
                   </FloatingLabel>
                   <br></br>
@@ -106,8 +195,8 @@ function MyProjects(props) {
                       type="date"
                       id="fechaInicial"
                       name="fechaInicial"
-                      value={formData.fechaInicial}
-                      onChange={handleInputChange}
+                      value={FechaInicio}
+                      onChange={(event) => setFechaIni(event.target.value)}
                     />
                   </FloatingLabel>
 
@@ -118,8 +207,8 @@ function MyProjects(props) {
                       type="date"
                       id="fechaFinal"
                       name="fechaFinal"
-                      value={formData.fechaFinal}
-                      onChange={handleInputChange}
+                      value={FechaFinalizacion}
+                      onChange={(event) => setFechaFin(event.target.value)}
                     />
                   </FloatingLabel>
 
@@ -130,8 +219,8 @@ function MyProjects(props) {
                       type="text"
                       id="costoEstimado"
                       name="costoEstimado"
-                      value={formData.costoEstimado}
-                      onChange={handleInputChange}
+                      value={CostoEstimado}
+                      onChange={(event) => setCosto(event.target.value)}
                     />
                   </FloatingLabel>
                   <Button variant="secondary" onClick={handleCloseModal}>
@@ -149,23 +238,24 @@ function MyProjects(props) {
             </Modal.Body>
           </Modal>
 
+          {/* Renderizado de proyectos */}
           {projectsData.map((formData, index) => (
             <div key={index}>
               <Card style={{ width: "700px" }}>
-                <Card.Header as="h5">{formData.name}</Card.Header>
+                <Card.Header as="h5">{formData.Nombre}</Card.Header>
                 <Card.Body>
                   <div>
-                    <Card.Title>{formData.name}</Card.Title>
                     <div style={{ display: "flex", "flex-direction": "row" }}>
                       <Card.Img
                         variant="top"
-                        src="https://d3ml3b6vywsj0z.cloudfront.net/company_images/5c3b00a0d55ae49f1b76b9ad_images.png"
+                        src="https://cdn-icons-png.flaticon.com/512/3381/3381007.png"
                         style={{ width: "100px" }}
                       />
-                      <Card.Text>{formData.description}</Card.Text>
+                      <Card.Text>{formData.Descripcion}</Card.Text>
                     </div>
                   </div>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary" onClick={() => handleEdit(formData._id)}>Editar</Button>
+                  <Button variant="secondary" onClick={() => handleDelete(formData._id)}>Eliminar</Button>
                 </Card.Body>
               </Card>
               <br></br>
